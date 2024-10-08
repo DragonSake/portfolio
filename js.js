f93b607faf695015f8e7e5fd0c4dacafef5b1bbf
DragonSake <40611000+DragonSake@users.noreply.github.com>
2024-10-08 20:17:54 +0200

    Delete js.js

--- js.js
+++
@@ -1,9 +0,0 @@
- window.addEventListener('scroll', function () {
-     var nav = document.querySelector('nav');
-
-     if (window.scrollY > 50) {
-         nav.style.backgroundColor = 'rgba(14, 11, 33, 0.8)';
-     } else {
-         nav.style.backgroundColor = 'transparent';
-     }
- });

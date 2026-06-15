1126f5215bb8bc2586283ced9ce5a21986cb5d69
DragonSake <40611000+DragonSake@users.noreply.github.com>
2026-06-15 23:08:58 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,9 +1,27 @@
- window.addEventListener('scroll', function () {
-     var nav = document.querySelector('nav');
+const navbar = document.querySelector('.navbar');
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const navLinks = document.getElementById('nav-links');
+const navItems = document.querySelectorAll('.nav-item');
 
-     if (window.scrollY > 50) {
-         nav.style.backgroundColor = 'rgba(14, 11, 33, 0.8)';
-     } else {
-         nav.style.backgroundColor = 'transparent';
-     }
- });
+// 1. Scroll Effect voor Desktop
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+// 2. Hamburger Menu Openen / Sluiten
+hamburgerBtn.addEventListener('click', () => {
+    hamburgerBtn.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
+// 3. Sluit menu wanneer je op een link klikt
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburgerBtn.remove('active');
+        navLinks.classList.remove('active');
+    });
+});
